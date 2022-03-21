b3e86951dcb438f832808abbdb0ce621e3cd5a1f
Serge <warwhisper@tut.by>
2022-03-21 17:11:28 +0300

    Collinear Points 1

---
+++ CollinearPoins.java
@@ -0,0 +1,11 @@
+/* *****************************************************************************
+ *  Name:              Searhei
+ *  Coursera User ID:  123456
+ *  Last modified:     March 21, 2022
+ **************************************************************************** */
+
+public class CollinearPoins {
+    public static void main(String[] args) {
+
+    }
+}
--- Shell.java
+++ Shell.java
@@ -8,7 +8,7 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Shell {
-    public static void sort(Comparable[] a) {
+    public static void sort(Comparable<Integer>[] a) {
         int n = a.length;
         int h = 1;
         while (h < n / 3) h = 3 * h + 1; // 1, 4, 13, 40, 121, 364, ...
@@ -30,17 +30,17 @@ public class Shell {
     //     h = 4 / 3 = 1;
     // }
 
-    private static boolean less(Comparable v, Comparable w) {
+    private static boolean less(Comparable<Integer> v, Comparable<Integer> w) {
         return v.compareTo(w) < 0;
     }
 
-    private static void exch(Comparable[] a, int i, int j) {
-        Comparable swap = a[i];
+    private static void exch(Comparable<Integer>[] a, int i, int j) {
+        Comparable<Integer> swap = a[i];
         a[i] = a[j];
         a[j] = swap;
     }
 
-    private static boolean isSorted(Comparable[] a) {
+    private static boolean isSorted(Comparable<Integer>[] a) {
         for (int i = 1; i < a.length; i++)
             if (less(a[i], a[i - 1])) return false;
         return true;
@@ -49,7 +49,7 @@ public class Shell {
     public static void main(String[] args) {
         String inFile = args[0];
         In in = new In(inFile);
-        int[] a = in.readAllInts();
+        Comparable<Integer>[] a = in.readAllInts();
         for (int s : a)
             StdOut.printf("%s ", s);
         StdOut.println();
